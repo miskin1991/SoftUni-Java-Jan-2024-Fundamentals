186d989810f2a2adaa2460e4ebf92f838970dc8a
Miroslav Stoychev <miskin@abv.bg>
2024-01-11 23:36:55 +0100

    Basic Types lab.

---
+++ BasicSyntaxLab/_08_DivisibleBy3.java
@@ -0,0 +1,11 @@
+package BasicSyntaxLab;
+
+public class _08_DivisibleBy3 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ BasicSyntaxLab/_09_SumOfOddNumbers.java
@@ -0,0 +1,20 @@
+package BasicSyntaxLab;
+
+import java.util.Scanner;
+
+public class _09_SumOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+        for (int i = 1; number > 0; i++) {
+            if ( i % 2 == 1) {
+                System.out.println(i);
+                sum += i;
+                number--;
+            }
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
